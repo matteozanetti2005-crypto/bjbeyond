9a5f569167878473029b723f05667fa6fac19266
BJ Beyond <matteo.zanetti2005@gmail.com>
2026-08-14 13:19:04 +0200

    Remove unused Microsoft Clarity script (placeholder ID)

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,21 +92,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </a>
         {children}
 
-        {/* Microsoft Clarity – replace YOUR_CLARITY_PROJECT_ID when ready */}
-        <Script
-          id="microsoft-clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "YOUR_CLARITY_PROJECT_ID");
-            `,
-          }}
-        />
-
         {/* Google Analytics GA4 – active */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-99P3F76R3D"
